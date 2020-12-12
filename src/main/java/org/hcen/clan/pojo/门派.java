c282ec9ea2deffc2121965fab483d4916ababe65
hcen1997 <panzetaoyx@gmail.com>
2020-12-12 10:58:39 +0800

    秀

--- src/main/java/org/hcen/clan/pojo/门派.java
+++ src/main/java/org/hcen/clan/pojo/门派.java
@@ -7,13 +7,13 @@ import java.util.List;
  * this is a clan
  * in which has many people called
  */
-public class 门派 {
-    private List<弟子> 门人;
-    private Double 杂役;
-    private Double 灵石;
-    private List<山峰> 设施;
+public class 门派 { // 公司
+    private List<弟子> 门人; // 领导
+    private Double 杂役; // 员工
+    private Double 灵石; // 资金
+    private List<山峰> 设施;  // List<部门> 设施
     private List<物品> 资源;
-    private Double 庇护民众;
+    private Double 庇护民众;  // 客户
 
 
 }

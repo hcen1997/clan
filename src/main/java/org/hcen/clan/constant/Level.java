1ac8afc16dccc0ca504e1d244b6c075a05a45698
hcen1997 <panzetaoyx@gamil.com>
2020-12-25 14:05:11 +0800

    能飞升啦~

--- src/main/java/org/hcen/clan/constant/Level.java
+++ src/main/java/org/hcen/clan/constant/Level.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @NoArgsConstructor
 public class Level {
+    // todo 让灵根等级决定最终成就
     public static final long[] MAP = new long[]{0L,
             100L, 500L, 1000L,
             2000L, 5000L, 10_000L,

ae80bf48b5f84a399a51e34f1dc9b279526e83dc
hcen1997 <panzetaoyx@gamil.com>
2020-12-25 15:13:08 +0800

    虽然我写的想哭，但是目前，1级灵根可以把自己出生的等级增加一个（靠着个人努力修行 0级灵根能修炼

--- src/main/java/org/hcen/clan/constant/Level.java
+++ src/main/java/org/hcen/clan/constant/Level.java
@@ -9,7 +9,7 @@ public class Level {
     // todo 让灵根等级决定最终成就
     // todo 1灵根4k年龄不可取
     private static final long[] MAP = new long[]{0L,
-            100L, 500L, 1000L,
+            500L, 1_000L, 1100L,
             2000L, 5000L, 10_000L,
             50_000L, 100_000L, 200_000L};
     private static Level one = new Level();

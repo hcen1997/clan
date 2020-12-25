3835b541ba1e747520256586dcbfac67298b174d
hcen1997 <panzetaoyx@gamil.com>
2020-12-25 13:56:59 +0800

    飞升者怎么会死呢？ 只是飞升而已 目前没有想到好的办法， 先转移吧

--- src/main/java/org/hcen/clan/World.java
+++ src/main/java/org/hcen/clan/World.java
@@ -12,7 +12,9 @@ import java.util.List;
 @Slf4j
 @Service
 public class World extends AbstractWorld {
-    private final List<人> 人s =人.born(10) ;
+    private final List<人> 人s = 人.born(10);
+    private final List<人> 飞升俱乐部 = new ArrayList<>();
+
 
     @Autowired
     private 飞升之路 飞升;
@@ -23,20 +25,30 @@ public class World extends AbstractWorld {
         var 死亡名单 = new ArrayList<String>();
         for (人 人 : 人s) {
             飞升.updateOne(人);
-            if(人.getLevel().isMax()){
-                log.info(String.format("飞升者 %s 出现了，今年是 %d",人.getName(),this.age));
+            if (人.getLevel().isMax()) {
+                log.info(String.format("飞升者 %s 出现了，今年是 %d", 人.getName(), this.age));
+                post.add(人.getName());
             }
-            if( 人.getAge()>人.getMaxAge()){
-                log.error(String.format("修炼者 %s 死于 %d , 最大年龄 %s",人.getName(),this.age,
+            if (人.getAge() > 人.getMaxAge()) {
+                log.error(String.format("修炼者 %s 死于 %d , 最大年龄 %s", 人.getName(), this.age,
                         人.getMaxAge()));
-                log.debug("死亡时的能量为"+人.getEnergy());
+                log.debug("死亡时的能量为" + 人.getEnergy());
                 死亡名单.add(人.getName());
             }
         }
         死亡名单.forEach(s -> {
             人s.removeIf(人 -> 人.getName().equals(s));
         });
-        if(人s.size()==0){
+        post.forEach(s -> {
+            人s.forEach(r -> {
+                if (r.getName().equals(s)) {
+                    飞升俱乐部.add(r);
+                }
+            });
+            人s.removeIf(人 -> 人.getName().equals(s));
+        });
+
+        if (人s.size() == 0) {
             setAllDead(true);
         }
     }
